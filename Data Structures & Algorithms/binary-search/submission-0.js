126cda1f0e35d12fba04098412fe033bbe98c9a3
Pavan Kumar <135621512+Pavan-Kumar-18@users.noreply.github.com>
2026-07-31 15:22:57 +0530

    Update: binary-search - submission-0

--- Data Structures & Algorithms/binary-search/submission-0.js
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -4,23 +4,21 @@ class Solution {
      * @param {number} target
      * @return {number}
      */
-    search(nums, target) {
-
-        let left = 0
+    search(nums, target) 
+    {
+        let left = 0 
         let right = nums.length-1
 
         while(left <= right){
-            let mid = Math.floor((left + right)/ 2)
-
+            let mid = Math.floor((left + right) / 2)
             if(nums[mid] === target){
                 return mid
             }else if(nums[mid] < target){
-                left = mid + 1
+                left = mid+1
             }else{
-               right = mid - 1
+                right = mid - 1
             }
         }
         return -1
-
     }
 }
